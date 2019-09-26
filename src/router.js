ec08a5259e3746f14ef2deba1812e75ba09f5666
lychees <lychees67@gmail.com>
2019-09-27 01:59:33 +0800

    More pages.

---
+++ src/router.js
@@ -0,0 +1,54 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: "",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/billboard",
+      name: "billboard",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/billboard.vue")
+    },
+    {
+      path: "/stuffs",
+      name: "stuffs",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/stuffs.vue")
+    },    
+    {
+      path: "/login/github",
+      name: "GithubLogin",
+      component: () => import("./views/GithubLogin.vue")
+    },
+    {
+      path: "/login/twitter",
+      name: "TwitterLogin",
+      component: () => import("./views/TwitterLogin.vue")
+    },
+  ]
+});
