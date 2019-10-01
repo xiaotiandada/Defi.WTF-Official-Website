860fcf1165eb3755c26fbd1e30384f19a34f917c
Frank Wei <hhwei98@gmail.com>
2019-10-01 16:18:00 +0800

    fix: missing components

--- src/App.vue
+++ src/App.vue
@@ -10,51 +10,10 @@
 <script>
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Billboard from "./components/Billboard.vue";
-import InfoBoard from "./components/InfoBoard/index.vue";
-
 
 export default {
   name: "APP",
-  components: { InfoBoard, Billboard, Header, Footer },
-  data() {
-    return {
-      title: "",
-      startPrice: "",
-      description: "",
-      amount: 0,
-      auctionCard: [],
-      auctionCardDev: [],
-      isShow: false,
-      isLoad: false,
-      isBid: false,
-      isFin: false,
-      bidPrice: "",
-      auctionAddress: "",
-      bidders: 0,
-      finalizeStatus: "Finalize auctioin",
-      createStatus: "CREATE",
-      adInfo: {
-        id: "ad0",
-        price: 600,
-        ratio: 0.02,
-        content:
-          "https://wx1.sinaimg.cn/large/70eb479bly1g7d68qao4fj20f408iwnp.jpg"
-      }
-    };
-  },
-  beforeMount() {
-  },
-  methods: {
-    buy() {     
-    },
-    createAuction() {
-    },
-    handleSubmit() {
-    },
-    handleFinalize() {
-    }
-  }
+  components: { Header, Footer },
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -262,3 +262,52 @@
     </section>
     </div>
 </template>
+
+<script>
+import Billboard from "../components/Billboard.vue";
+import InfoBoard from "../components/InfoBoard/index.vue";
+
+
+export default {
+  name: "Home",
+  components: { InfoBoard, Billboard },
+  data() {
+    return {
+      title: "",
+      startPrice: "",
+      description: "",
+      amount: 0,
+      auctionCard: [],
+      auctionCardDev: [],
+      isShow: false,
+      isLoad: false,
+      isBid: false,
+      isFin: false,
+      bidPrice: "",
+      auctionAddress: "",
+      bidders: 0,
+      finalizeStatus: "Finalize auctioin",
+      createStatus: "CREATE",
+      adInfo: {
+        id: "ad0",
+        price: 600,
+        ratio: 0.02,
+        content:
+          "https://wx1.sinaimg.cn/large/70eb479bly1g7d68qao4fj20f408iwnp.jpg"
+      }
+    };
+  },
+  beforeMount() {
+  },
+  methods: {
+    buy() {     
+    },
+    createAuction() {
+    },
+    handleSubmit() {
+    },
+    handleFinalize() {
+    }
+  }
+};
+</script>
