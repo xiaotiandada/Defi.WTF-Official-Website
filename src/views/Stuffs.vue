7ccfb7b7501ee320e4c00516f013aca878d5c8cd
xiazhiming <358200167@qq.com>
2019-10-01 18:48:14 +0800

    add tina

--- src/views/Stuffs.vue
+++ src/views/Stuffs.vue
@@ -1,25 +1,12 @@
 <template>
 <section class="stuffs">
  <div class="container ">
- <h2>标题名称</h2>
+ <h2>Community member</h2>
  <div class="row stuffs-card">
 
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
-<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><img src="src/assets/img/stuffs.png"><p class="stuffs-name">名字</P></div>
+<div class=" col-xl-1 col-md-2  col-sm-4 col-4 stuffs-item "><a  target="_blank" href="https://github.com/CarboClanC"><img src="src/assets/img/stuffs/tina.jpeg"></a><p class="stuffs-name">Tina,Zhen</P></div>
+
+
 
 </div>
  </div>
