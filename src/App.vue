f667f08f234d77aedca8d382edd5f54ceeae73f1
Frank Wei <hhwei98@gmail.com>
2019-10-01 16:11:27 +0800

    feat: add footer component

--- src/App.vue
+++ src/App.vue
@@ -261,30 +261,20 @@
             <a target="_blank" href="https://twitter.com/intent/tweet?text=defi&url=https%3A%2F%2Fwww.defi.wtf%2F" class="share-twitter"><i class="fa fa-twitter"></i></a>
         </div>
     </section>
-    <footer class="footer-section spad set-bg" data-setbg="src/assets/img/footer-bg.svg">
-        <div class="container">
-            <div class="social-links">
-                <a target="_blank" href="https://t.me/defiwtf" class="telegram wow fadeInDown" data-wow-delay="0.2s"><i class="fa fa-telegram "></i></a>
-                <a href="https://twitter.com/WTFdao" class="twitter wow fadeInDown" data-wow-delay="0.4s"><i class="fa fa-twitter"></i></a>
-                <a target="_blank" href="https://github.com/carboclan/pm/issues/69" class="github wow fadeInDown" data-wow-delay="0.4s"><i class="fa fa-github"></i></a>
-                <a target="_blank" href="mailto:ask@defi.wtf" class="envelope wow fadeInDown" data-wow-delay="0.4s"><i class="fa fa-envelope"></i></a>
-            </div>
-            <!-- copyright -->
-            <p> <a href="http://www.DeFi.WTF.com/" title="DeFi.WTF." target="_blank">© 2019 DeFi.WTF.</a></p>
-        </div>
-    </footer>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Billboard from "./components/Billboard.vue";
 import InfoBoard from "./components/InfoBoard/index.vue";
 
 
 export default {
   name: "APP",
-  components: { InfoBoard, Billboard, Header },
+  components: { InfoBoard, Billboard, Header, Footer },
   data() {
     return {
       title: "",
---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+        <footer class="footer-section spad set-bg" data-setbg="src/assets/img/footer-bg.svg">
+        <div class="container">
+            <div class="social-links">
+                <a target="_blank" href="https://t.me/defiwtf" class="telegram wow fadeInDown" data-wow-delay="0.2s"><i class="fa fa-telegram "></i></a>
+                <a href="https://twitter.com/WTFdao" class="twitter wow fadeInDown" data-wow-delay="0.4s"><i class="fa fa-twitter"></i></a>
+                <a target="_blank" href="https://github.com/carboclan/pm/issues/69" class="github wow fadeInDown" data-wow-delay="0.4s"><i class="fa fa-github"></i></a>
+                <a target="_blank" href="mailto:ask@defi.wtf" class="envelope wow fadeInDown" data-wow-delay="0.4s"><i class="fa fa-envelope"></i></a>
+            </div>
+            <!-- copyright -->
+            <p> <a href="http://www.DeFi.WTF.com/" title="DeFi.WTF." target="_blank">© 2019 DeFi.WTF.</a></p>
+        </div>
+    </footer>
+</template>
