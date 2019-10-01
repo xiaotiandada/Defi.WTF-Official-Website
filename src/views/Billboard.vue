7d8529fe5da46e75f87e697001cc4f453fcb9bcf
lychees <lychees67@gmail.com>
2019-10-01 19:03:07 +0800

    Add contract json

--- src/views/Billboard.vue
+++ src/views/Billboard.vue
@@ -3,8 +3,8 @@
     <div class="container">
       <div class="row billboard">
         <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img :src="1" alt="cover[0]"> </div>
-        <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img :src="2" alt="cover[1]"> </div>
-        <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img src="src/assets/img/wtf5.png"></div>
+        <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img :src="cover" alt="cover[1]"> </div>
+        <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img :src="adlist[0]"></div>
         <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img src="src/assets/img/wtf5.png"></div>
         <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img src="src/assets/img/wtf5.png"></div>
         <div class="col-xl-1-5 col-lg-3 col-md-4  col-xs-6 col-sm-6 "> <img src="src/assets/img/wtf5.png"></div>
@@ -16,3 +16,60 @@
     </div>
   </section> 
 </template>
+
+<script>
+export default {
+  components: {
+  },
+  props: [
+  ],
+  data(){
+    return {
+      cover: '',
+      text: '',
+      url1: '',
+      adlist: '',
+    }
+  },
+  computed: {
+  
+  },
+  watch: {
+    parseURL(newVal) {
+      let val = JSON.parse(newVal)
+      if (val.cover) this.cover = val.cover
+      if (val.text) this.text = val.text
+    }
+  },
+  created(){
+  },
+  destoryed(){
+  },
+  methods: {
+    getHeight:function() {
+    },
+    getData: async function(adId) {
+      let adboardData = await this.$root.getAdBoardData(adId);
+      this.artName = adboardData.content;
+    },
+    getAdBoardData: async function(total) {
+      for (let i = 0; i < total; i++) {
+        try {
+          let res = await this.$root.getAdBoardDataId(i)
+          console.log('res', res)
+          // arr.push(res)
+          this.adList.push(res)
+        } catch (error) {
+          console.log('getAdBoardDataId', error)
+        }
+      }
+    },
+    getTotalNumber: async function () {
+      await this.$root.getTotalNumber().then(res => {
+        this.getAdBoardData(Number(res))
+      })
+    }
+
+  }
+};
+</script>
