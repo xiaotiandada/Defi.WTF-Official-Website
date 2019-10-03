8e3c8832ac931fdaf586819ddb694e0174657e74
lychees <lychees67@gmail.com>
2019-10-03 23:45:40 +0800

    delete unused code

--- src/views/BuyBillboard.vue
+++ src/views/BuyBillboard.vue
@@ -227,20 +227,6 @@ export default {
           console.log('getAdBoardDataId', error)
         }
       }
-
-      switch (netId) {
-        case "1":
-          console.log('This is mainnet')
-          break
-        case "2":
-          console.log('This is the deprecated Morden test network.')
-          break
-        case "3":
-          console.log('This is the ropsten test network.')
-          break
-        default:
-          console.log('This is an unknown network.')
-      }
     })
   },
   destroyed(){
