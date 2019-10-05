b5d1730cffdc7e3515e6b4b1a5d8d224af722731
管超 <1242256806@qq.com>
2019-10-05 18:29:59 +0800

    Revert "update agenda & speakers"
    This reverts commit bb1f4ce87c452b761944613ef4808ed1419f56e5.

--- src/util/people.js
+++ src/util/people.js
@@ -8,21 +8,17 @@
 */
 
 export default{
+  Tina: {
+    image: "tina.jpeg",
+    link: "https://twitter.com/tzhen"
+  },
+  Fran: {
+    image: "fran.jpeg",
+    link: "https://twitter.com/francescorenzia"
+  },
   FelixFeng: {
     image: "Felix Feng @Set Protocol.jpg",
     link: "https://www.linkedin.com/in/felix-feng/"
-  },
-  DianeDai: {
-    image: "Diane Dai @CypherJump.jpg",
-    link: "https://www.linkedin.com/in/shichao-dai-649890a7/"
-  },
-  CamilaRusso: {
-    image: "Camila Russo @the Defiant.jpg",
-    link: "https://www.linkedin.com/in/camila-russo-87009b1b/"
-  },
-  SunnyAggarwal: {
-    image: "Sunny Aggarwal @Cosmos.jpg",
-    link: "https://www.linkedin.com/in/sunnya97/"
   },
   TomSchmidt: {
     image: "Tom Schmidt @0x.jpg",
@@ -32,117 +28,9 @@ export default{
     image: "Alex Evans @Placeholder.jpg",
     link: "https://www.linkedin.com/in/alexhevans/"
   },
-  LevLivnev: {
-    image: "Lev Livnev @DappHub.jpg",
-    link: "https://github.com/livnev"
-  },
-  ChristianCrowley: {
-    image: "Christian Crowley @Alethio.jpg",
-    link: "https://twitter.com/ChrisCCrowley"
-  },
-  ShrutiAppiah: {
-    image: "Shruti Appiah.jpg",
-    link: "https://www.linkedin.com/in/shrutiappiah/"
-  },
-  JamesPreswich: {
-    image: "James Preswich @Summa @tBTC.jpg",
-    link: "https://www.linkedin.com/in/prestwich/"
-  },
-  TInaZhen: {
-    image: "TIna Zhen @YellowHatDAO.jpg",
-    link: "https://www.linkedin.com/in/tina-zhen-5185b324/"
-  },
-  IlliaPolosukhim: {
-    image: "Illia Polosukhim @Near.jpg",
-    link: "https://www.linkedin.com/in/illia-polosukhin-77b6538"
-  },
-  JakeKim: {
-    image: "Jake Kim @Everett.jpg",
-    link: "https://www.linkedin.com/in/jake-jae-won-kim-324084165/"
-  },
-  Mindao: {
-    image: "Mindao @dForce.jpg",
-    link: "https://www.linkedin.com/in/ziggy-zeidan-64a93565/"
-  },
-  Bowen: {
-    image: "Bowen @DDEX.jpg",
-    link: "https://www.linkedin.com/in/bowenwang992/"
-  },
-  Victor: {
-    image: "Victor @Alphawallet.jpg",
-    link: "http://linkedin.com/in/zhangzhongnan"
-  },
-  ClaireBelmont: {
-    image: "Claire Belmont @Celo Protocol.jpg",
-    link: "https://www.linkedin.com/in/clairebelmont/"
-  },
-  SujiYan: {
-    image: "Suji Yan @Dimension.jpg",
-    link: "https://www.linkedin.com/in/tedkoyan/"
-  },
-  DuyHuyn: {
-    image: "Duy Huyn @Incognito.jpg",
-    link: "https://www.linkedin.com/in/duyhtq/"
-  },
-  PatrickGallagher: {
-    image: "Patrick Gallagher & Victor Rortvedt @rdai dao.jpg",
-    link: "https://www.linkedin.com/in/victor-rortvedt-1978608/"
-  },
-  Jieliu: {
-    image: "Jie liu @Monte Carlo.jpg",
-    link: "https://www.linkedin.com/in/jie-liu-34380337/"
-  },
-  KristofferNærland: {
-    image: "Kristoffer Nærland @Trustlines Network.jpg",
-    link: "https://www.linkedin.com/in/kristoffer-n%C3%A6rland-562ab46b/"
-  },
-  FernandoMartinelli: {
-    image: "Fernando Martinelli @Balancer Labs.jpg",
-    link: "https://www.linkedin.com/in/fernandomartinelli/"
-  },
-  MaksimBalashevich: {
-    image: "Maksim Balashevich @Santiment.net.jpg",
-    link: "https://www.linkedin.com/in/maksim-balashevich-640a8948/"
-  },
-  SiddigZeidan: {
-    image: "Siddig Zeidan  @POA Network and xDai Chain team.jpg",
-    link: "https://www.linkedin.com/in/ziggy-zeidan-64a93565/"
-  },
-  JennaZenk: {
-    image: "Jenna Zenk @MelonCouncil @AvantgardeFinance.jpg",
-    link: "https://www.linkedin.com/in/jenna-zenk-72545266/"
-  },
-  AmeenSoleimani: {
-    image: "Ameen Soleimani @MolochDAO & @Spankchain.jpg",
-    link: "https://www.linkedin.com/in/ameen-soleimani-97181942/"
-  },
-  PeterPan: {
-    image: "Peter Pan @MetaCartel.jpg",
-    link: "https://twitter.com/pet3rpan_"
-  },
-  JorgeIzquierdo: {
-    image: "Jorge Izquierdo @Aragon.jpg",
-    link: "https://www.linkedin.com/in/izqui/"
-  },
-  MartinKoppelman: {
-    image: "Martin Koppelman @Gnosis @Dxdao.jpg",
-    link: "https://www.linkedin.com/in/koeppelmann/"
-  },
-  JackClancy: {
-    image: "Jack Clancy @CoDeFi.jpg",
-    link: "https://www.linkedin.com/in/jack-clancy-454843a8/"
-  },
-  ChiachiWu: {
-    image: "Chiachi Wu @Peckshield.jpg",
-    link: "https://www.linkedin.com/in/chiachih-wu-78449528/"
-  },
   JoelMonegro: {
     image: "Joel Monegro @Placeholder.jpg",
-    link: "https://twitter.com/jmonegro"
-  },
-  MableJiang: {
-    image: "Mable Jiang @Nirvana Cap.png",
-    link: "https://www.linkedin.com/in/mable-jiang/"
+    link: ""
   },
   ChrisMcCann: {
     image: "Chris McCann @Proof of Capital.jpg",
@@ -152,65 +40,61 @@ export default{
     image: "Lasse Clausen @1kx.jpg",
     link: "https://www.linkedin.com/in/lasseclausen/"
   },
-  RichardChen: {
-    image: "Richard Chen @1confirmation.jpg",
-    link: "https://www.linkedin.com/in/rchen8/"
+  TinAmeenSoleimania: {
+    image: "Ameen Soleimani @MolochDAO.jpg",
+    link: "https://www.linkedin.com/in/ameen-soleimani-97181942/"
   },
-  HaseebQureshi: {
-    image: "Haseeb Qureshi @Dragonfly Capital.jpg",
-    link: "https://www.linkedin.com/in/imhaseeb/"
+  JennaZenk: {
+    image: "",
+    link: "https://www.linkedin.com/in/jenna-zenk-72545266/"
   },
-  NirajPant: {
-    image: "Niraj Pant @Polychain.jpg",
-    link: "https://www.linkedin.com/in/nirajpant/"
+  PeterPan: {
+    image: "Peter Pan @MetaCartel.jpg",
+    link: "https://www.linkedin.com/in/peter-pan/"
   },
-  DanRobinson: {
-    image: "Dan Robinson @Paradigm.jpg",
+  JorgeIzquierdo: {
+    image: "",
+    link: "https://www.linkedin.com/in/izqui/"
+  },
+  DanielRobinson: {
+    image: "",
     link: "https://www.linkedin.com/in/danielprobinson/"
   },
   ZhuoxunYin: {
     image: "Zhuoxun Yin @Dydx.jpg",
-    link: "https://www.linkedin.com/in/zhuoxun-yin-3ba93728/"
+    link: "linkedin.com/in/zhuoxun-yin-3ba93728/"
   },
   TrentElmore: {
     image: "Trent Elmore @Topo Finance.jpg",
     link: "https://www.linkedin.com/in/trentelmore/"
   },
-  JacobShiach: {
-    image: "Jacob Shiach @Metamoneymarket.jpg",
-    link: "https://www.linkedin.com/in/jacobshiach/"
+  SunnyAggarwal: {
+    image: "",
+    link: "https://www.linkedin.com/in/sunnya97/"
   },
-  KyleKistner: {
-    image: "Kyle Kistner @bZx.jpg",
-    link: "https://www.linkedin.com/in/kyleb0x/"
+  RichardChen: {
+    image: "",
+    link: "https://www.linkedin.com/in/rchen8/"
+  },
+  NirajPant: {
+    image: "",
+    link: "https://www.linkedin.com/in/nirajpant/"
   },
   BowenWang: {
-    image: "Bowen Wang @DDex.jpg",
+    image: "",
     link: "https://www.linkedin.com/in/bowenwang992/"
   },
   AllisonLu: {
-    image: "Allison Lu @UMA.jpg",
+    image: "",
     link: "https://www.linkedin.com/in/allisonlu/"
-  },
-  Kyle: {
-    image: "Kyle @Bzx.jpg",
-    link: "https://www.linkedin.com/in/kyleb0x/"
-  },
-  MindaoYang: {
-    image: "Mindao Yang @dForce @USDx.jpg",
-    link: "https://www.linkedin.com/in/ziggy-zeidan-64a93565/"
-  },
-  SeeEun: {
-    image: "See Eun @Nonce.jpg",
-    link: "https://twitter.com/s3unha"
   },
   TonySheng: {
     image: "Tony Sheng @Multicoin.jpg",
     link: "https://www.linkedin.com/in/tonysheng/"
   },
-  AndrewAlbertson: {
+  CanSun: {
     image: "",
-    link: "https://www.linkedin.com/in/andrew-albertson-9b47563b/"
+    link: "https://www.linkedin.com/in/can-sun/"
   },
   ItamarLesuisse: {
     image: "Itamar Lesuisse @Argent.jpg",
@@ -220,28 +104,92 @@ export default{
     image: "Hayden Adams @Uniswap.jpg",
     link: "https://www.linkedin.com/in/haydenadams/"
   },
-  VadimKoleoshkin: {
-    image: "Vadim Koleoshkin @Zerion.jpg",
-    link: "https://www.linkedin.com/in/vadim-koleoshkin/"
-  },
   AndrewKeys: {
     image: "Andrew Keys @DARMA Capital.jpg",
     link: "https://www.linkedin.com/in/andrew-keys-45a9517/"
-  },
-  PhilipSeifert: {
-    image: "",
-    link: ""
   },
   ChaoPan: {
     image: "Chao Pan @MakerDAO China.jpg",
     link: "https://www.linkedin.com/in/chao-pan-30294b86/"
   },
-  SunCan: {
+  SunnyAggarwal: {
     image: "",
-    link: "https://www.linkedin.com/in/can-sun/"
+    link: "https://www.linkedin.com/in/sunnya97/"
+  },
+  KyleKistner: {
+    image: "Kyle Kistner @Bzx.jpg",
+    link: "https://www.linkedin.com/in/kyleb0x/"
+  },
+  CamilaRusso: {
+    image: "",
+    link: ""
+  },
+  DianeDai: {
+    image: "",
+    link: ""
+  },
+  LoiLu: {
+    image: "",
+    link: ""
+  },
+  MableJiang: {
+    image: "",
+    link: ""
+  },
+  HaseebQureshi: {
+    image: "",
+    link: ""
+  },
+  LevLivnev: {
+    image: "",
+    link: ""
+  },
+  MartinKoppelman: {
+    image: "",
+    link: ""
+  },
+  JacobShiach: {
+    image: "",
+    link: ""
+  },
+  JamesPreswich: {
+    image: "",
+    link: ""
+  },
+  MartinKoppelman: {
+    image: "",
+    link: ""
+  },
+  PhilipSeifert: {
+    image: "",
+    link: ""
+  },
+  CamilaRusso: {
+    image: "",
+    link: ""
+  },
+  JackClancy: {
+    image: "",
+    link: ""
+  },
+  ChiachiWu: {
+    image: "",
+    link: ""
+  },
+  ShrutiAppiah: {
+    image: "",
+    link: ""
+  },
+  ChristianCrowley: {
+    image: "",
+    link: ""
   },
   KarlFloerach: {
-    image: "Karl Floerach.jpg",
-    link: "https://www.linkedin.com/in/karlfloersch/"
+    image: "",
+    link: ""
+  },
+  ShrutiAppiah: {
+    image: "",
+    link: ""
   }
 }
